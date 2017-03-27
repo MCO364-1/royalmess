b90b9b216c6089d60e82ffe7288de352ac3f752c
dcohen24 <dcohen24@KG-403.academic.touro.edu>
2017-03-27 17:24:26 -0400

    Merge origin/master
    Conflicts:
    	Royalmess/src/main/java/mco364/EntryPoint.java

---
+++ Royalmess/src/main/java/mco364/DoviCohen.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mco364;
+
+/**
+ *
+ * @author dcohen24
+ */
+public class DoviCohen extends SuperClass{
+
+    @Override
+    public String toString() {
+        return "its a bird....its a plane.... its Dovi Cohen extends SuperClass";
+    }
+    
+}
