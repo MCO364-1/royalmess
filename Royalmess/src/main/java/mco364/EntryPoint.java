f20f705a0db07d32113cf6c70faf4baf2b4ceaff
rabraham3 <rabraham3@KG-401.academic.touro.edu>
2017-03-27 17:14:00 -0400

    Merge origin/master
    Conflicts:
    	Royalmess/src/main/java/mco364/EntryPoint.java

--- Royalmess/src/main/java/mco364/EntryPoint.java
+++ Royalmess/src/main/java/mco364/EntryPoint.java
@@ -3,7 +3,6 @@ package mco364;
 public class EntryPoint {
 
     public static void main(String[] args) {
-        System.out.println("Problem Child");
         System.out.println("Dr. Robinson");
         System.out.println("Yisroel Perton");
         System.out.println("Reuven Markov");
@@ -11,5 +10,6 @@ public class EntryPoint {
         System.out.println("Yehuda Gruen");
         System.out.println("Rafi Abrahamson");
         System.out.println("Dirty Bit");
+        System.out.println("just pushed ");
     }
 }
