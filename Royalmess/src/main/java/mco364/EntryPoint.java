6394b8d417d85431d76a57084403ea84adeee066
Yehuda <Yehuda@10.150.44.195>
2017-03-27 17:47:21 -0400

    Merge origin/master
    Conflicts:
    	Royalmess/src/main/java/mco364/EntryPoint.java

--- Royalmess/src/main/java/mco364/EntryPoint.java
+++ Royalmess/src/main/java/mco364/EntryPoint.java
@@ -6,22 +6,19 @@ public class EntryPoint {
 
     public static void main(String[] args) {
         ArrayList<SuperClass> superMen = new ArrayList<>();
-        
+
         superMen.add(new SuperClass());
         superMen.add(new MegaFafnik());
         superMen.add(new SuperYisroel());
         superMen.add(new losev());
         superMen.add(new losev());
         //Hey
-       superMen.add(new losev());
-       superMen.add(new LiffClass());
-        
-        
-        for (Object o: superMen)
-        {
+        superMen.add(new losev());
+        superMen.add(new LiffClass());
+
+        for (Object o : superMen) {
             System.out.println(o);
         }
 
-        
     }
 }
