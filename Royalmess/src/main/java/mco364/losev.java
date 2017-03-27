c8ba394381a76cd6d1397c43082a78c6ccf30dc6
markLosev <moshe.losev@yahoo.com>
2017-03-27 17:23:20 -0400

    tgedg

---
+++ Royalmess/src/main/java/mco364/losev.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mco364;
+
+/**
+ *
+ * @author Mark
+ */
+public class losev extends SuperClass {
+    @Override
+    public String toString() {
+        return "whats up kido";
+    }
+}
