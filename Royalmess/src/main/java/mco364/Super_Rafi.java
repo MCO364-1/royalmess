1b0624b0668e7ff8ae201ec70c037041358847f0
rabraham3 <rabraham3@KG-401.academic.touro.edu>
2017-03-27 17:24:02 -0400

    Merge origin/master
    Conflicts:
    	Royalmess/src/main/java/mco364/EntryPoint.java

---
+++ Royalmess/src/main/java/mco364/Super_Rafi.java
@@ -0,0 +1,15 @@
+package mco364;
+
+/**
+ *
+ * @author rabraham3
+ */
+public class Super_Rafi extends SuperClass{
+    String field = " the devil is in the details";
+    public Super_Rafi(){
+    field=" the devil is in the details";
+    }
+    
+    @Override
+    Public String 
+}
--- Royalmess/src/main/java/mco364/rafi.java
+++
@@ -1,9 +0,0 @@
-package mco364;
-
-/**
- *
- * @author rabraham3
- */
-public class rafi extends SuperClass{
-    
-}
